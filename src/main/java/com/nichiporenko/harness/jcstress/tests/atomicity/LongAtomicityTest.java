2ebd08924067ecf0d49b005b6884c52befc9ba71
nichiporenko <excelsiorious@gmail.com>
2019-09-24 18:07:34 +0300

    Added atomicity tests (long, double)

---
+++ src/main/java/com/nichiporenko/harness/jcstress/tests/atomicity/LongAtomicityTest.java
@@ -0,0 +1,27 @@
+package com.nichiporenko.harness.jcstress.tests.atomicity;
+
+import org.openjdk.jcstress.annotations.*;
+import org.openjdk.jcstress.infra.results.J_Result;
+
+/*
+ * Of course, in order to see the partial write operation to the long field, this test must be run on a 32-bit JVM.
+ */
+@JCStressTest
+@Outcome(id = "0", expect = Expect.ACCEPTABLE, desc = "Default value for the field.")
+@Outcome(id = "-1", expect = Expect.ACCEPTABLE, desc = "The value set. Observer sees the full update.")
+@Outcome(id = "", expect = Expect.ACCEPTABLE_INTERESTING, desc = "Observer sees the torn value.")
+@State
+public class LongAtomicityTest {
+
+    long v;
+
+    @Actor
+    public void actor1(J_Result r) {
+        v = -1L;
+    }
+
+    @Actor
+    public void actor2(J_Result r) {
+        r.r1 = v;
+    }
+}
---
+++ src/main/java/com/nichiporenko/harness/jcstress/tests/atomicity/VolatileDoubleAtomicityTest.java
@@ -0,0 +1,27 @@
+package com.nichiporenko.harness.jcstress.tests.atomicity;
+
+import org.openjdk.jcstress.annotations.*;
+import org.openjdk.jcstress.infra.results.D_Result;
+
+/*
+ * Of course, in order to see the partial write operation to the double field, this test must be run on a 32-bit JVM.
+ */
+@JCStressTest
+@Outcome(id = "0.0", expect = Expect.ACCEPTABLE, desc = "Default value for the field.")
+@Outcome(id = "123.12345678", expect = Expect.ACCEPTABLE, desc = "The value set. Observer sees the full update.")
+@Outcome(id = "", expect = Expect.FORBIDDEN, desc = "Observer sees the torn value.")
+@State
+public class VolatileDoubleAtomicityTest {
+
+    volatile double v;
+
+    @Actor
+    public void actor1(D_Result r) {
+        v = 123.12345678;
+    }
+
+    @Actor
+    public void actor2(D_Result r) {
+        r.r1 = v;
+    }
+}
